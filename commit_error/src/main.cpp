723749646c207e19f26d78de3b420d1f55e499b9
DavidFriedl79 <ddavidffriedl@gmail.com>
2025-11-17 14:26:45 +0100

    int to float

--- commit_error/src/main.cpp
+++ commit_error/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int myFunction(float, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int result = myFunction(1, 2);
 }
 
 void loop() {
@@ -13,6 +13,6 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(float x, int y) {
   return x + y;
 }
